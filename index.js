c9880564eaf3450dd7819ed30abb78b64cb1cbd1
BraKwaku Jr <brakwakujr@BraKwakus-MacBook-Pro.local>
2020-08-22 17:26:57 -0600

    Commit trying to fix heroku problem

--- index.js
+++ index.js
@@ -24,8 +24,6 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const PORT = process.env.PORT || 3000 // So we can run on heroku || (OR) localhost:3000
 
-// const io = require('socket.io')() // For live chat on index page
-
 const app = express();
 
 // Route setup. You can implement more in the future!
